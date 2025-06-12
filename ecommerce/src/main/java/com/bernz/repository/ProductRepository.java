6b623a431e21fb5263c5f394f5eb93ceaf169e6c
Bernie <baltazarbernie05@gmail.com>
2025-06-12 22:47:45 +0800

    add ProductController

--- ecommerce/src/main/java/com/bernz/repository/ProductRepository.java
+++ ecommerce/src/main/java/com/bernz/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.bernz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.bernz.model.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+// JpaSpecificationExecutor - It allows you to write dynamic, type-safe queries using the JPA Criteria API.
+//                          - You can filter, sort, and paginate results based on complex, runtime conditions.
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     
 }
--- ecommerce/src/main/java/com/bernz/service/impl/ProductServiceImpl.java
+++ ecommerce/src/main/java/com/bernz/service/impl/ProductServiceImpl.java
@@ -1,9 +1,14 @@
 package com.bernz.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.bernz.exceptions.ProductException;
@@ -15,6 +20,9 @@ import com.bernz.repository.ProductRepository;
 import com.bernz.request.CreateProduct;
 import com.bernz.service.ProductService;
 
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -23,7 +31,6 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
     
-    // 7:51:43
     public int calculateDiscountPercentage(int mrpPrice, int sellingPrice) {
         if (mrpPrice <= 0) {
             throw new IllegalArgumentException("Actual price must be greater than 0");
@@ -90,34 +97,85 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void deleteProduct(Long productId) throws ProductException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteProduct'");
+        Product product = findProductById(productId);
+        productRepository.delete(product);
     }
 
     @Override
     public Product updateProduct(Long productId, Product product) throws ProductException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateProduct'");
+        findProductById(productId);
+        product.setId(productId);
+        return productRepository.save(product);
     }
 
     @Override
     public Product findProductById(Long productId) throws ProductException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findProductById'");
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new ProductException("Product not found with id: " + productId));
     }
 
     @Override
     public List<Product> searchProducts(String name) throws ProductException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'searchProducts'");
+        return List.of();
     }
 
     @Override
     public Page<Product> getAllProducts(String category, String brand, String colors, String size, Integer minPrice,
             Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber)
             throws ProductException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllProducts'");
+                
+            // Specification - composable query conditions for your entitie
+            // Predicate - It represents a single condition in a query (e.g., "price > 100", "category = 'Shoes'")
+            // Join - Used to join related entities in a Criteria query (like SQL JOIN)
+            Specification<Product> spec = (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (category != null) {
+                Join<Product, Category> categoryJoin = root.join("category");
+                predicates.add(criteriaBuilder.equal(categoryJoin.get("categoryId"), category));
+            }
+
+            if (colors != null && !colors.isEmpty()) {
+                predicates.add(criteriaBuilder.equal(root.get("color"), colors));
+            }
+
+            if (size != null && !size.isEmpty()) {
+                predicates.add(criteriaBuilder.equal(root.get("size"), size));
+            }
+
+            if (minPrice != null) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("selligPrice"), minPrice));
+            }
+
+            if (maxPrice != null) {
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("sellingPrice"), maxPrice));
+            }
+
+            if (minDiscount != null) {
+                predicates.add(criteriaBuilder.equal(root.get("discountPercentage"), minDiscount));
+            }
+
+            if (stock != null) {
+                predicates.add(criteriaBuilder.equal(root.get("stock"), stock));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+
+        // Filtering
+        Pageable pageable;
+        if (sort != null && !sort.isEmpty()) {
+            pageable = switch (sort) {
+                case "price_low" -> PageRequest.of(pageNumber != null ? pageNumber : 0, 10, Sort.by("sellingPrice").ascending());
+                case "price_high" -> PageRequest.of(pageNumber != null ? pageNumber : 0, 10, Sort.by("sellingPrice").descending());
+                default -> PageRequest.of(pageNumber != null ? pageNumber : 0, 10, Sort.unsorted());
+            };
+        }
+        else {
+            pageable = PageRequest.of(pageNumber != null ? pageNumber : 0, 10, Sort.unsorted());
+        }
+
+        return productRepository.findAll(spec, pageable);
     }
 
     @Override
