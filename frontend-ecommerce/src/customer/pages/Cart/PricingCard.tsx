3b12d4f7c296db56db8fffac8a15811234865626
Bernie <baltazarbernie05@gmail.com>
2025-07-22 14:10:04 +0800

    add cart and cart item

--- frontend-ecommerce/src/customer/pages/Cart/PricingCard.tsx
+++ frontend-ecommerce/src/customer/pages/Cart/PricingCard.tsx
@@ -1,8 +1,36 @@
+import { Divider } from '@mui/material'
 import React from 'react'
 
 const PricingCard = () => {
   return (
-    <div>PricingCard</div>
+    <div className='px-5 py-3 space-y-4'>
+        <div className='flex justify-between item-center'>
+            <p>SubTotal</p>
+            <span>PHP 1399</span>
+        </div>
+
+        <div className='flex justify-between item-center'>
+            <p>Discount</p>
+            <span>PHP 600</span>
+        </div>
+
+        <div className='flex justify-between item-center'>
+            <p>Shipping</p>
+            <span>PHP 79</span>
+        </div>
+
+        <div className='flex justify-between item-center'>
+            <p>Plateform fee</p>
+            <span>Free</span>
+        </div>
+
+        <Divider />
+
+        <div className='flex justify-between item-center p-1'>
+            <p>Total</p>
+            <span>PHP 799</span>
+        </div>
+    </div>
   )
 }
 
