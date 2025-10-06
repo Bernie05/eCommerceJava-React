0a39ee9721bd0867e1e6ba0848ed638e1b512b37
BernieBaltazar <bernie.baltazar@pnp-hcl.com>
2025-10-06 18:43:08 +0800

    add table for coupon and dashboard

--- frontend-ecommerce/src/admin/pages/coupon/Coupon.tsx
+++ frontend-ecommerce/src/admin/pages/coupon/Coupon.tsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import React, { useState } from "react";import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TableCell, { tableCellClasses }  from '@mui/material/TableCell';
+import { Delete } from "@mui/icons-material";
 
-const Coupon = () => {
-  return (
-    <div>Coupon</div>
-  )
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default Coupon
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export const accStatus = [
+  {
+    status: "PENDING_VERIFICATION",
+    title: "Pending Verification",
+    description: "",
+  },
+  {
+    status: "ACTIVE",
+    title: "Active",
+    description: "",
+  },
+  {
+    status: "SUSPENDED",
+    title: "Suspended",
+    description: "",
+  },
+  {
+    status: "DEACTIVATED",
+    title: "Deactivated",
+    description: "",
+  },
+  {
+    status: "BANNED",
+    title: "Banned",
+    description: "",
+  },
+  {
+    status: "CLOSED",
+    title: "Close",
+    description: "",
+  },
+];
+const Coupon = () => {
+  const [accountStatus, setAccountStatus] = useState("ACTIVE");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAccountStatus(event.target.value as string);
+  };
+
+  return (
+    <>
+      <div className="pb-5 w-60">
+        <FormControl fullWidth>
+          <InputLabel id="account-status">Account Status</InputLabel>
+          <Select
+            labelId="account-status"
+            id="account-status"
+            label="Account Status"
+            value={accountStatus}
+            onChange={handleChange}
+          >
+            {accStatus.map(({ status, title }) => (
+              <MenuItem value={status}>{title}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* table */}
+      <div>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Coupon Code</StyledTableCell>
+                <StyledTableCell align="left">Start Date</StyledTableCell>
+                <StyledTableCell align="right">End Date</StyledTableCell>
+                <StyledTableCell align="right">Min Order value</StyledTableCell>
+                <StyledTableCell align="right">Discount %</StyledTableCell>
+                <StyledTableCell align="right">Status</StyledTableCell>
+                <StyledTableCell align="right">Delete</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">{row.calories}</StyledTableCell>
+                  <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                  <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                  <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                  <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    <Button>
+                      <Delete />
+                    </Button>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </>
+  );
+};
+
+export default Coupon;
