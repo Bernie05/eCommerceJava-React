c756f8b8713417373022f098aefb541b2c016309
BernieBaltazar <bernie.baltazar@pnp-hcl.com>
2025-10-15 18:46:29 +0800

    add seller api integration

---
+++ frontend-ecommerce/src/state/seller/selletAuthSlice.ts
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ILoginRequest } from "../auth";
+import { api } from "../../config/Api";
+
+export const sellerLogin = createAsyncThunk<any, ILoginRequest>('/seller/sellerLogin', async(data: ILoginRequest) => {
+    const { email, otp } = data;
+
+    if (!email || !otp) {
+        throw new Error("Email and OTP are required");
+    }
+    try {
+        const response = await api.post('/sellers/login', {
+            email,
+            otp,
+        });
+
+        console.log('sellerLogin res: ', response.data);
+        return response.data;
+    } catch(err) {
+        console.error("Failed to fetch auth profile", err);
+        throw err;
+    }
+});
